118a57baaed74ccad27e9334f68e2d34e50fff5d
Matheus <matheus>
2021-07-26 19:12:29 -0300

    feat: add valid email and implementation formik and yup

--- src/pages/ProfileEdit/editEmail/index.tsx
+++ src/pages/ProfileEdit/editEmail/index.tsx
@@ -1,150 +1,135 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, TouchableOpacity, Modal} from "react-native"
+import React from 'react'
+import { View, Text, TouchableOpacity} from "react-native"
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { TextInput } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/core';
 
 import styles from '../styles';
 import stylesGlobal from '../../styles-global'
+import { useFormik} from 'formik';
+import { editEmailForm } from './editEmail.form';
 
 
 const ProfileEditEmail = () => {
-    const [disableButton, setDisableButton] = useState<boolean>(true);
-    const [opacityButton, setOpacityButton] = useState<number>(0.5);
-    const[email, setEmail] = useState<string>('');
-    const[confirmEmail, setConfirmEmail] = useState<string>('');
-    const [visibleModalOne, setVisibleModalOne ] = useState<boolean>(false);
-    const [visibleModalTwo, setVisibleModalTwo ] = useState<boolean>(false);
-
-    useEffect(() => {
-        if(!!email && !!confirmEmail){
-            setDisableButton(false)
-            setOpacityButton(1)
-        }else {
-            setDisableButton(true)
-            setOpacityButton(0.5)
-        }
-    })
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            confirmEmail: '',
+            disabledButton: true
+        },
+        validationSchema: editEmailForm,
+        onSubmit: values => {
+            console.log(values)
+            alterData()
+        },
+    });
 
     const navigation = useNavigation();
 
-    function navigateBack(){
-        navigation.goBack()
-    }
-
     function alterData(){
         //enviar dados para serem alterados
-        console.log('alterado: ', email);
-        setVisibleModalOne(false)
+        console.log('alterado: ', formik.values.email)
         navigation.navigate('Home')
-
     }
-
-    function emailEqual(){
-        if(email.toLowerCase() === confirmEmail.toLowerCase()){
-            setVisibleModalOne(true);
-        }else {
-            setVisibleModalTwo(true);
-        }
-    }
-
+    formik.setFieldValue
     return (
         <> 
             <View 
                 style={{backgroundColor: '#fff'}}
             >
-                <View style={styles.container}>
-                    <View style={styles.action}>
+                <View style={{...styles.container, backgroundColor: '#fff'}}>
+                    <View style={{...styles.action, borderBottomColor: formik.touched.email && formik.errors.email ? 'red' :'#F2F2F2'}}>
                         <Icon 
                             name='email' size={30} 
-                            color='#605C99'
+                            color={ formik.touched.email && formik.errors.email ? 'red' :'#605C99' }
                         />
                         <TextInput 
                             placeholder='E-mail'
                             placeholderTextColor='#666666'
                             keyboardType='email-address'
                             autoCorrect={false}
-                            onChangeText={(val) => setEmail(val)}
+                            onFocus={() => formik.setFieldTouched('email')}
+                            onChangeText={formik.handleChange('email')}
                             style={{marginLeft: 20, fontSize: 18}}
                         />
-
                     </View>
-                    <View style={styles.action}>
+                    {
+                        formik.touched.email && formik.errors.email ?
+                        <View
+                            style={{
+                                alignItems: 'flex-start'
+
+                            }}
+                        >
+                            <Text testID="error-email" style={
+                                {
+                                    color: "white", 
+                                    backgroundColor: "red",
+                                    fontSize: 16,
+                                    marginHorizontal: 20,
+                                    padding: 2,
+                                    borderRadius: 3,
+                                }
+                            }>
+                                {formik.errors.email}
+                            </ Text>
+                        </View>
+                        : null
+                    }
+                    <View style={{...styles.action, borderBottomColor: formik.touched.confirmEmail && formik.errors.confirmEmail ? 'red' :'#F2F2F2'}}>
                         <Icon 
                             name='email' size={30} 
-                            color='#605C99'
+                            color={formik.touched.confirmEmail && formik.errors.confirmEmail ? 'red' :'#605C99'}
                         />
                         <TextInput 
                             placeholder='Confirmar E-mail'
                             placeholderTextColor='#666666'
                             keyboardType='email-address'
                             autoCorrect={false}
-                            onChangeText={(val) => setConfirmEmail(val)}
+                            onFocus={() => formik.setFieldTouched('confirmEmail')}
+                            onChangeText={formik.handleChange('confirmEmail')}
                             style={{marginLeft: 20, fontSize: 18}}
                         />
 
                     </View>
+                    {
+                        formik.touched.confirmEmail && formik.errors.confirmEmail ?
+                        <View
+                            style={{
+                                alignItems: 'flex-start'
 
-                    
+                            }}
+                        >
+                            <Text testID="error-email" style={
+                                {
+                                    color: "white", 
+                                    backgroundColor: "red",
+                                    fontSize: 16,
+                                    marginHorizontal: 20,
+                                    padding: 2,
+                                    borderRadius: 3,
+                                }
+                            }>
+                                {formik.errors.confirmEmail}
+                            </ Text>
+                        </View>
+                        : null
+                    }
+                    {
+                        console.log('aqqui', formik.touched.email === undefined? true : !formik.isValid)
+                    }
                     <View style={{alignItems: 'center'}}>
                         <TouchableOpacity 
-                            style={{...stylesGlobal.button, opacity: opacityButton}}
-                            onPress={emailEqual}
-                            disabled={disableButton}
+                            style={{...stylesGlobal.button, opacity: formik.touched.email === undefined? 0.5 : !formik.isValid ? 0.5: 1}}
+                            onPress={() => formik.handleSubmit()}
+                            disabled={formik.touched.email === undefined? true : !formik.isValid  }
                         >
                             <Text style={stylesGlobal.buttonText}>Salvar</Text>
                         </TouchableOpacity>
 
                     </View>
-                    
                 </View>
-                
-                
             </View>
-            
-            <Modal
-                animationType='slide'
-                transparent={true}
-                visible={visibleModalOne}
-            >
-                <View style={styles.modal}>
-                    <Text style={styles.title}>Quer realmente alterar seu E-mail?</Text>
-                    <View style={styles.buttonArea}>
-                        <TouchableOpacity 
-                            style={{...styles.buttonModal, marginHorizontal: 10}}
-                            onPress={() => alterData()}
-                        >
-                            <Text style={stylesGlobal.buttonText}>Sim</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity 
-                            style={styles.buttonModal}
-                            onPress={() => setVisibleModalOne(false) }
-                        >
-                            <Text style={stylesGlobal.buttonText}>Não</Text>
-                        </TouchableOpacity>
-                    </View>
-    
-                </View>
-            </Modal>
-
-            <Modal
-                animationType='slide'
-                transparent={true}
-                visible={visibleModalTwo}
-            >
-                <View style={styles.modal}>
-                    <Text style={styles.title}>Os E-mails não estão iguais!</Text>
-                    <View style={styles.buttonArea}>
-                        <TouchableOpacity 
-                            style={styles.buttonModal}
-                            onPress={() => setVisibleModalTwo(false) }
-                        >
-                            <Text style={stylesGlobal.buttonText}>Ajustar</Text>
-                        </TouchableOpacity>
-                    </View>
-    
-                </View>
-            </Modal>
         </>
     )
 }
